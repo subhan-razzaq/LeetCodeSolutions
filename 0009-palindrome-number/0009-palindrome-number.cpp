d1f181f375a58e12569825af351fd81df81887b3
Subhan Razzaq <subhanrazzaq777@gmail.com>
2026-01-16 16:54:11 -0500

    Time: 5 ms (20.95%), Space: 10.9 MB (15.01%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string Converted = to_string(x);
+        string check = "";
+        bool palindrome = false;
+        for (int i = Converted.length() - 1; i >= 0; i--)
+        {
+            check += Converted[i];
+        }
+        if (check == Converted)
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+
+    }
+};
