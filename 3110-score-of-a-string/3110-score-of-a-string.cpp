b4e94cc3600cc12da31e343f3852050bfb2a31fb
Subhan Razzaq <subhanrazzaq777@gmail.com>
2026-01-17 14:23:29 -0500

    Time: 0 ms (100%), Space: 8.7 MB (99.44%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.cpp
+++ 3110-score-of-a-string/3110-score-of-a-string.cpp
@@ -2,10 +2,13 @@ class Solution {
 public:
     int scoreOfString(string s) {
     int score = 0;
+
+    // go through string and add absolute difference between ASCII values to score
     for (int i = 0; i < s.length() - 1; i++)
     {
         score += abs(s[i] - s[i+1]);
     }
+
     return score;
     }
 };
