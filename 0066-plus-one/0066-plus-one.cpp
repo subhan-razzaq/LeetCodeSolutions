0a0b4a97cf4150de3a1347c10577060f1e071228
Subhan Razzaq <subhanrazzaq777@gmail.com>
2026-01-20 22:36:24 -0500

    Time: 0 ms (100%), Space: 11.4 MB (76.19%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+    digits.back()++;
+    for (int i = digits.size() - 1; i >= 0; i--)
+    {
+        if (digits[i] < 10)
+        {
+            break;
+        }
+        digits[i] = 0;
+        if (i == 0)
+        {
+            digits[i] = 0;
+            digits.insert(digits.begin(), 1);
+        }
+        else
+        {
+            digits[i - 1]++;
+            digits[i] = 0;
+        }
+    }
+    return digits;
+    }
+};
