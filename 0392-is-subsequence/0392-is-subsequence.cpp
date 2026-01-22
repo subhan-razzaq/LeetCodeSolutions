9a8bc2e614f1948812596d8eeb71d1d7be815fbe
Subhan Razzaq <subhanrazzaq777@gmail.com>
2026-01-21 21:07:12 -0500

    Time: 0 ms (100%), Space: 8.6 MB (31.72%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
+    if (s.length() == 0)
+    {
+        return true;
+    }
     int indexSubsequence = 0;
-    string check = "";
     for (int i = 0; i < t.length(); i++)
     {
         if (t[i] == s[indexSubsequence])
         {
-            check += s[indexSubsequence];
             indexSubsequence++;
+            if (indexSubsequence == s.length())
+            {
+                return true;
+            }
         }
     }
-    if (check == s)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+    return false;
     }
 };
